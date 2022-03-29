ba4e54a27f923a7964eee12dec53f0534e0805e6
Poly Titova <poly_titova@mail.ru>
2022-03-29 18:07:38 +0300

    Добавили списки

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 // Набор фильмов
 const movies = [
@@ -8,6 +8,10 @@ const movies = [
   { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
   { id: '3', name: 'V for vendetta', genre: 'Sci-Fi-Triller', directorId: '3' },
   { id: '4', name: 'Snatch', genre: 'Crime-Comedy', directorId: '4' },
+  { id: '5', name: 'Reservoir Dogs', genre: 'Crime', directorId: '1' },
+  { id: '6', name: 'The Hateful Eight', genre: 'Crime', directorId: '1' },
+  { id: '7', name: 'Inglourious Basterds', genre: 'Crime', directorId: '1' },
+  { id: '8', name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime-Comedy', directorId: '4' },
 ];
 
 // Набор режисёров
@@ -42,6 +46,12 @@ const DirectorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter(movie => movie.directorId == parent.id);
+      },
+    },
   }),
 });
 
@@ -64,6 +74,18 @@ const Query = new GraphQLObjectType({
       resolve(parent, args) {
         return directors.find(director => director.id == args.id);
       },
+    },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies;
+      }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      resolve(parent, args) {
+        return directors;
+      }
     },
   }
 });
